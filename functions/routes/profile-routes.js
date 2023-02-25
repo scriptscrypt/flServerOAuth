1e44c077f47379fe5acd874c19feab1b6775e5e4
scriptscrypt <tirumaladasa@gmail.com>
2023-02-25 22:19:25 +0530

    remoteV1.4 25Feb2023

--- functions/routes/profile-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authCheck = require("./conditionals").authCheck
-
-router.get("/view", authCheck, (req, res)=>{
-    res.render("profile", {varName : req.user.keyUserName, varGoogleId : req.user.keyGoogleId})
-})
-
-
-module.exports = router;
